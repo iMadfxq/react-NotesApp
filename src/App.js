b9ac7571a12a3470e1ab47efb02cfb1506af779d
iMadfxq <imadfxq@gmail.com>
2022-05-14 11:17:31 -0400

    Finished adding all the buttons that I will be using in the app, with it's corresponding switch cases

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import { BUTTON_TYPES } from './components/button/button.component';
 function App() {
   return (
     <>
-      <Button type={BUTTON_TYPES.newNote} />
-      <Button type={BUTTON_TYPES.noteDisplayMode} />
+      <Button type={BUTTON_TYPES.NEW_NOTE} />
+      <Button type={BUTTON_TYPES.DISPLAY_MODE} />
+      <Button type={BUTTON_TYPES.DONE} />
+      <Button type={BUTTON_TYPES.HOME} />
+      <Button type={BUTTON_TYPES.EDIT} />
+      <Button type={BUTTON_TYPES.CLOSE} />
+      <Button type={BUTTON_TYPES.SMALL_DELETE} />
+      <Button type={BUTTON_TYPES.SMALL_EDIT} />
+      <Button type={BUTTON_TYPES.SMALL_ARCHIVE} />
     </>
   );
 }
--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,27 +1,46 @@
-import "./button.scss"
+import "./button.scss";
 
 export const BUTTON_TYPES = {
-  "newNote": "newNote",
-  "noteDisplayMode": "noteDisplayMode",
+  NEW_NOTE: "NEW_NOTE",
+  DISPLAY_MODE: "DISPLAY_MODE",
+  DONE: "DONE",
+  HOME: "HOME",
+  EDIT: "EDIT",
+  CLOSE: "CLOSE",
+  SMALL_DELETE: "SMALL_DELETE",
+  SMALL_EDIT: "SMALL_EDIT",
+  SMALL_ARCHIVE: "SMALL_ARCHIVE",
+};
 
-}
-
-const Button = ({type}) => {
-  switch(type) {
-    case BUTTON_TYPES.newNote:
-      return (
-        <button>New Note</button>
-      )
-    case BUTTON_TYPES.noteDisplayMode:
+const Button = ({ type }) => {
+  switch (type) {
+    case BUTTON_TYPES.NEW_NOTE:
+      return <button>New Note</button>;
+    case BUTTON_TYPES.DISPLAY_MODE:
       return (
         <div>
           <button>squares</button>
           <button>columns</button>
         </div>
-      )
+      );
+    case BUTTON_TYPES.DONE:
+      return <button>&#x2713; Done</button>;
+    case BUTTON_TYPES.HOME:
+      return <button>&#x21aa; Home</button>;
+    case BUTTON_TYPES.EDIT:
+      return <button>✏️ Edit</button>;
+    case BUTTON_TYPES.CLOSE:
+      return <button>&#x2715;</button>;
+      case BUTTON_TYPES.SMALL_DELETE:
+        return <button>🗑</button>;
+        case BUTTON_TYPES.SMALL_EDIT:
+        return <button>✏️</button>;
+        case BUTTON_TYPES.SMALL_ARCHIVE:
+        return <button>📁</button>;
+  
     default:
-      throw new Error("Your button had no type, or an unrecognized type.")
+      throw new Error("Your button had no type, or an unrecognized type.");
   }
-}
+};
 
-export default Button
+export default Button;
