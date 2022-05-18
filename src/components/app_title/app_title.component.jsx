2da57fbb4016c758223f34750ea18b815eb983c0
iMadfxq <imadfxq@gmail.com>
2022-05-17 23:56:36 -0400

    Started creating the logic

--- src/components/app_title/app_title.component.jsx
+++ src/components/app_title/app_title.component.jsx
@@ -1,20 +1,21 @@
 import "./app_title.styles.scss";
 
-import { Link } from "react-router-dom";
-
-import Button from '../button/button.component'
+import Button from "../button/button.component";
 import { BUTTON_TYPES } from "../button/button.component";
 
+const handleNewNoteClick = () => {
+  console.log("clicked");
+};
 
 const AppTitle = () => {
   return (
     <section className="header__title">
       <h1>Notes - React App</h1>
-      <Link to={"/create"}>
+      <div onClick={handleNewNoteClick}>
         <Button type={BUTTON_TYPES.NEW_NOTE} />
-      </Link>
+      </div>
     </section>
   );
 };
 
-export default AppTitle
+export default AppTitle;
