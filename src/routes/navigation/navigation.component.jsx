6016785bf92c7cae953df5e9d32ced3ed501c6a5
iMadfxq <imadfxq@gmail.com>
2022-05-23 14:53:15 -0400

    Added my footer, small style improvements

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -7,6 +7,7 @@ import { useSelector } from "react-redux";
 import TabSelector from "../../components/tab_selector/tab_selector.component";
 import AppTitle from "../../components/app_title/app_title.component";
 import NoteCreator from "../../components/note_creator/note_creator.component";
+import Footer from "../../components/footer/footer.component";
 
 const Navigation = () => {
   const creatingNote = useSelector((state) => state.creatingNote)
@@ -18,6 +19,7 @@ const Navigation = () => {
         <TabSelector />
       </header>
       <Outlet />
+      <Footer portfolio={'https://imadfxq.com/'}/>
     </>
   );
 };
